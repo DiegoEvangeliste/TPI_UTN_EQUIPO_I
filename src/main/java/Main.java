8fb8cd8a74980c63fb431c06400a30e291246cba
HouMengZambrano <houmeng.zambrano@gmail.com>
2023-11-14 21:56:54 -0300

    modificacion del path de los archivos pronostico y resultados

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,8 +6,8 @@ public class Main {
     public static void main(String[] args) {
         ProdeController prodeController = new ProdeController();
         try{
-            prodeController.juego("/home/diego/Documentos/Programacion/TPI_UTN_EQUIPO_I/src/main/resources/pronosticos.cvs",
-                        "/home/diego/Documentos/Programacion/TPI_UTN_EQUIPO_I/src/main/resources/resultados.cvs");
+            prodeController.juego("TPI_UTN_EQUIPO_I/src/main/resources/pronosticos.cvs",
+                        "TPI_UTN_EQUIPO_I/src/main/resources/resultados.cvs");
         }
         catch(FileNotFoundException e){
             new RuntimeException(e);
